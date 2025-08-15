e3d80c26ec057535a966b7b9f861e94620ed0c67
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-08-15 14:38:48 -0400

    Update main.tf
    Updated AMI ID with ami-0a7d80731ae1b2435 and subnetID

--- test/fixtures/simple/main.tf
+++ test/fixtures/simple/main.tf
@@ -8,9 +8,9 @@ module "under_test" {
   source = "../../.."
 
   name                  = "tf-test-ec2-123456"
-  ami_id                = "ami-00000000000000000" # replace when running real tests
+  ami_id                = "ami-0a7d80731ae1b2435" # replace when running real tests
   instance_type         = "t3.micro"
-  subnet_id             = "subnet-00000000000000000"
+  subnet_id             = "subnet-0ac19a8ca72369eef"
   create_security_group = false
   vpc_security_group_ids = []
   tags                  = { env = "test" }
